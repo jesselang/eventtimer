fbf3e5cbd2c236fd18e38d2df67275e9ce0a4182
Jesse Lang <jesse@jesselang.com>
2011-05-29 22:11:59 -0500

    Add a README that looks a lot like a TODO. Re-style the code in a more readable fashion.

--- display.py
+++ display.py
@@ -6,28 +6,29 @@ import gobject
 import timer
 import Xlib.display
 
-white = clutter.Color (255, 255, 255, 255)
-black = clutter.Color(0,0,0,255)
-green = clutter.Color (0, 128, 0, 255)
-yellow = clutter.Color (200, 200, 0, 255)
-orange = clutter.Color (255, 128, 0, 255)
-red = clutter.Color(255, 0, 0, 255)
+# Colors
+white  = clutter.Color (255, 255, 255, 255)
+black  = clutter.Color (  0,   0,   0, 255)
+green  = clutter.Color (  0, 128,   0, 255)
+yellow = clutter.Color (200, 200,   0, 255)
+orange = clutter.Color (255, 128,   0, 255)
+red    = clutter.Color (255,   0,   0, 255)
 
+# Objects
 input = timer.TimeInput ()
 timer = timer.Timer ()
 
 alternate_black = False
+absolute_time   = False
 
-absolute_time = False
-
-def update_timer (timeline=None):
+def update_timer (timeline = None):
     if timer.running ():
-        time_elapsed.set_text (timer.timeelapsed () )
-        time_remaining.set_text (timer.timeremaining () )
+        time_elapsed.set_text (timer.elapsed () )
+        time_remaining.set_text (timer.remaining () )
 
         progress = timer.progress ()
 
-        if timer.secondsremaining () <= 60:
+        if timer.seconds_remaining () <= 60:
             global alternate_black
             alternate_black = not alternate_black
 
@@ -35,7 +36,7 @@ def update_timer (timeline=None):
                 stage.set_color (black)
             else:
                 stage.set_color (red)
-        elif timer.duration <= datetime.timedelta (minutes=20):
+        elif timer.duration <= datetime.timedelta (minutes = 20):
             if 0.0 <= progress <= 0.4:
                 stage.set_color (green)
             elif 0.4 <= progress <= 0.6:
@@ -55,31 +56,31 @@ def update_timer (timeline=None):
                 stage.set_color (red)
     else:
         stage.set_color (black)
-        time_remaining.set_text (input.tostring () )
+        time_remaining.set_text (input.to_string () )
 
         if absolute_time:
             time_elapsed.set_text (time.strftime ("%H:%M:%S", time.localtime () ) )
             # Show the absolute labels?
         else:
-            time_elapsed.set_text (timer.timeelapsed () )
+            time_elapsed.set_text (timer.elapsed () )
 
-def parseKeyPress(self, event):
+def parseKeyPress (self, event):
     global absolute_time
-    character_key = chr(0)
+    character_key = chr (0)
 
     if 0 <= event.keyval <= 255:
-        character_key = chr(event.keyval)
+        character_key = chr (event.keyval)
 
     if event.keyval == clutter.keysyms.q or event.keyval == clutter.keysyms.Q:
-        clutter.main_quit()
+        clutter.main_quit ()
     elif event.keyval == clutter.keysyms.space:
         if timer.running ():
             timer.reset ()
         else:
             if absolute_time:
-                timer.setduration (input.durationUntil ())
+                timer.set (input.durationUntil () )
             else:
-                timer.setduration (input.duration ())
+                timer.set (input.duration () )
 
             timer.start ()
     elif event.keyval == clutter.keysyms.at:
@@ -90,53 +91,53 @@ def parseKeyPress(self, event):
         if absolute_time:
             absolute_time = False
             input.clear ()
-    elif character_key.isdigit():
+    elif character_key.isdigit ():
         input.append (chr (event.keyval) )
         update_timer ()
     else:
         print 'What to do with: ',  event.keyval
 
-def redraw (fullscreen=False):
+def redraw (fullscreen = False):
     stage.set_fullscreen (fullscreen)
     stage_width, stage_height = stage.get_size ()
 
     update_timer ()
     text_width, text_height = time_remaining.get_size ()
 
-    stage.add(time_elapsed)
-    stage.add(time_remaining)
+    stage.add (time_elapsed)
+    stage.add (time_remaining)
 
     time_elapsed.set_position (stage_width / 2 - text_width / 2, stage_height / 3 - text_height / 2)
     time_remaining.set_position (stage_width / 2 - text_width / 2, stage_height / 3 * 2 - text_height / 2)
 
 def redraw_fullscreen ():
-    redraw (fullscreen=True)
+    redraw (fullscreen = True)
 
-stage = clutter.Stage()
+stage = clutter.Stage ()
 
 screen = Xlib.display.Display ().screen ()
 stage.set_size (screen.width_in_pixels, screen.height_in_pixels)
 
-stage.set_color(black)
-stage.connect('key-press-event', parseKeyPress)
-time_elapsed = clutter.Text()
+stage.set_color (black)
+stage.connect ('key-press-event', parseKeyPress)
+time_elapsed = clutter.Text ()
 # Dynamically set the font size based on screen size?
-time_elapsed.set_font_name("Sans " + str (screen.height_in_pixels / 4) + "px")
-time_elapsed.set_color(white)
+time_elapsed.set_font_name ("Sans " + str (screen.height_in_pixels / 4) + "px")
+time_elapsed.set_color (white)
 
 
-time_remaining = clutter.Text()
+time_remaining = clutter.Text ()
 # Dynamically set the font size based on screen size?
-time_remaining.set_font_name("Sans " + str (screen.height_in_pixels / 4) + "px")
-time_remaining.set_color(white)
+time_remaining.set_font_name ("Sans " + str (screen.height_in_pixels / 4) + "px")
+time_remaining.set_color (white)
 
 stage.show_all ()
 
-t=clutter.Timeline()
-t.set_duration(250)
-t.set_loop(True)
-t.connect('completed', update_timer)
-t.start()
+t=clutter.Timeline ()
+t.set_duration (250)
+t.set_loop (True)
+t.connect ('completed', update_timer)
+t.start ()
 
-gobject.timeout_add(10,redraw)
-clutter.main()
+gobject.timeout_add (10,redraw)
+clutter.main ()
--- timer.py
+++ timer.py
@@ -1,116 +1,144 @@
 import datetime
 import time
-import array
 import enum
 
-
-def total_seconds (td):
+# Helpful functions used by this package.
+def total_seconds (timedelta):
    # Included in timedelta starting in Python 2.7.
-   return float (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+   return float (timedelta.microseconds + (timedelta.seconds + timedelta.days * 24 * 3600) * 10 ** 6) / 10 ** 6
 
-def FormattedTime (delta):
-   minutes = delta.seconds / 60
-   return "%02d:%02d:%02d" % (minutes       / 60,
-                              minutes       % 60,
-                              delta.seconds % 60)
+def formatted_time (timedelta):
+   minutes = timedelta.seconds / 60
+   return "%02d:%02d:%02d" % (minutes           / 60,
+                              minutes           % 60,
+                              timedelta.seconds % 60)
 
-TimerState = enum.Enum (["Stopped", "Running", "Completed"])
+# State enumeration used by the Timer class.
+Timer_State = enum.Enum (["stopped", "running", "completed"])
 
 class Timer:
-   def __init__(self):
+   def __init__ (self):
       self.reset ()
+
    def start (self):
-      self.state = TimerState.Running
+      self.state = Timer_State.running
       self.started = datetime.datetime.now ()
       # We add a fudge second to make the time remaining look correct,
       # in place of displaying the ceiling of the fractional seconds.
       if self.duration is not None:
-         self.ending = self.started + self.duration + datetime.timedelta(seconds=1)
+         self.ending = self.started + self.duration + datetime.timedelta (seconds = 1)
+
    def reset (self):
-      self.state    = TimerState.Stopped
+      self.state    = Timer_State.stopped
       self.started  = None
       self.duration = None
       self.ending   = None
+
+   def update_state (self): # Internal operation.
+      if self.state == Timer_State.running and self.ending is not None and datetime.datetime.now () >= self.ending:
+         self.state = Timer_State.completed
+
    def running (self):
-      return self.state == TimerState.Running
-      #return self.started is not None
-   def setduration (self, duration):
-      if self.state == TimerState.Running:
+      return self.state == Timer_State.running
+
+   def set (self, duration):
+      if self.state == Timer_State.running:
          raise Exception ("Can't set duration while running.")
       else:
          self.duration = duration
-   def timeelapsed(self):
-      if self.state == TimerState.Completed:
-         return FormattedTime(self.duration)
-      if self.ending is not None and datetime.datetime.now () >= self.ending:
-         self.state = TimerState.Completed
 
-         return FormattedTime(self.duration)
+   def elapsed (self):
+      self.update_state ()
+
+      # Examine to see if only the time state alone can be used in the conditions.
+      if self.state == Timer_State.completed:
+         return formatted_time (self.duration)
       elif self.started is None:
-         return FormattedTime(datetime.timedelta (0))
+         return formatted_time (datetime.timedelta (0) )
       else:
-         return FormattedTime(datetime.datetime.now() - self.started)
-   def timeremaining(self):
-      if self.ending is not None and datetime.datetime.now () >= self.ending:
-         self.state = TimerState.Completed
+         return formatted_time (datetime.datetime.now () - self.started)
 
-         return FormattedTime(datetime.timedelta(0))
+   def remaining (self):
+      self.update_state ()
+
+      # Examine to see if only the time state alone can be used in the conditions.
+      if self.state == Timer_State.completed:
+         return formatted_time (datetime.timedelta (0) )
       elif self.started is None and self.duration is not None:
-         return FormattedTime(self.duration);
+         return formatted_time (self.duration);
       elif self.ending is None:
-         return FormattedTime(datetime.timedelta(0))
+         return formatted_time (datetime.timedelta (0) )
       else:
-         return FormattedTime(self.ending - datetime.datetime.now())
-   def progress(self):
-      if datetime.datetime.now () >= self.ending:
-         self.state = TimerState.Completed
+         return formatted_time (self.ending - datetime.datetime.now () )
+
+   def progress (self):
+      self.update_state ()
+
+      # Examine to see if only the time state alone can be used in the conditions.
+      if self.state == Timer_State.completed:
+         return 1.0
       elif self.started is None or self.duration is None or self.duration == 0:
          return 0.0
       else:
-         return total_seconds(datetime.datetime.now() - self.started) / total_seconds(self.duration)
-   def secondsremaining (self):
-      if datetime.datetime.now () >= self.ending:
-         self.state = TimerState.Completed
-      elif self.started is None or self.duration is None:
+         return total_seconds (datetime.datetime.now () - self.started) / total_seconds (self.duration)
+
+   def seconds_remaining (self):
+      self.update_state ()
+
+      # Examine to see if only the time state alone can be used in the conditions.
+      if self.started is None or self.duration is None or self.state != Timer_State.running:
          return 0
       else:
-         return total_seconds(self.ending - datetime.datetime.now() )
+         return total_seconds(self.ending - datetime.datetime.now () )
+
    def __str__(self):
+      self.update_state ()
+
       return "Time elapsed: " + self.timeelapsed () + "\n" + \
              "Time remaining: " + self.timeremaining() + "\n" + \
-             ("Progress: %2f" % self.progress())
+             ("Progress: %2f" % self.progress() )
 
 class TimeInput:
-   def __init__(self):
+   def __init__ (self):
       self.clear ()
+
    def clear (self):
-      self.digits = '0'*6
-   def append(self, key):
+      self.digits = '0' * 6
+
+   def append (self, key):
       if key.isdigit ():
-         self.digits = self.digits[1:] + key
+         self.digits = self.digits [1:] + key
       else:
          raise ValueError ("Could not append '" + key + "' to TimeInput object")
-   def duration(self):
-      return datetime.timedelta(hours=int(self.digits[0:2]),minutes=int(self.digits[2:4]),seconds=int(self.digits[4:6]) )
+
+   def duration (self):
+      return datetime.timedelta \
+         (hours = int (self.digits [0:2]), minutes = int (self.digits [2:4]), seconds = int (self.digits [4:6]) )
+
    def durationstring (self):
-      return FormattedTime (self.duration () )
+      return formatted_time (self.duration () )
+
    def durationUntil (self):
       # self.duration represents a target time, not a time delta.
-      target_seconds = int(total_seconds (self.duration () ) )
+      target_seconds = int (total_seconds (self.duration () ) )
       target_time = datetime.datetime.now ().replace \
-         (hour=target_seconds / 60 / 60, minute=target_seconds / 60 % 60, second=target_seconds % 60)
+         (hour = target_seconds / 60 / 60, minute = target_seconds / 60 % 60, second = target_seconds % 60)
+
       if target_time <= datetime.datetime.now ():
          raise ValueError ("Time entered has already occured.")
-      return target_time - datetime.datetime.now()
-   def tostring (self):
+
+      return target_time - datetime.datetime.now ()
+
+   def to_string (self):
       return self.digits [0:2] + ':' + self.digits [2:4] + ':' + self.digits [4:6]
-   def __str__(self):
-      return self.tostring ()
+
+   def __str__ (self):
+      return self.to_string ()
 
 if __name__ == '__main__':
    print "Timer demo"
    timer1 = Timer ()
-   timer1.setduration(datetime.timedelta(minutes=10) )
+   timer1.set (datetime.timedelta (minutes = 10) )
    print timer1
    timer1.start ()
    for I in range (10):
